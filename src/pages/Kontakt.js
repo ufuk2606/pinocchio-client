b860dc714083b713cba2da206482db099f15ff13
ufuk2606 <ufukozkar11@gmail.com>
2023-06-08 16:31:02 +0200

    kontakt mail added

--- src/pages/Kontakt.js
+++ src/pages/Kontakt.js
@@ -1,15 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import PageTop from "../components/PageTop";
 import Footer from "../components/Footer";
+import { UserContext } from "../contexts/UserContext";
+import { api } from "../services/httpService";
 
 function Kontakt() {
-  useEffect(()=>{
-    window.scroll({top: 0, left: 0, behavior: 'smooth' })
-  },[])
+  useEffect(() => {
+    window.scroll({ top: 0, left: 0, behavior: "smooth" });
+  }, []);
   const image = "images/kontakt-top.jpg";
   const title = "Kontakt";
   const content =
     "Gerne nehmen wir Ihre Fragen und Wünsche telefonisch oder per E-Mail entgegen und melden uns schnellstmöglich.";
+  const { currentUser } = useContext(UserContext);
+  const [mitteilung, setMitteilung] = useState("");
+
+  const createKontakt = async (pEmail) => {
+    const kontaktMail = {
+      mitteilung: mitteilung,
+    };
+    try {
+      const response = await api.post(
+        `/kontakt/kontakt?email=${pEmail}`,
+        kontaktMail
+      );
+      setMitteilung("");
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
   return (
     <>
       <div className="container-fluid">
@@ -20,46 +42,19 @@ function Kontakt() {
           <div className="col-8 mb-5">
             <h1 className="reservation-title">Kontaktanfrage</h1>
             <form className="me-5">
-              <div className="my-3 mt-5 ">
-                <div className="input-group">
-                  <input
-                    type="text"
-                    placeholder="Vorname*"
-                    className="form-control me-4 p-2"
-                    required
-                  />
-                  <input
-                    type="text"
-                    placeholder="Nachname*"
-                    className="form-control p-2"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="my-3 mt-4">
-                <div className="input-group">
-                  <input
-                    type="tel"
-                    placeholder="Telefon*"
-                    className="form-control me-4 p-2"
-                    required
-                  />
-                  <input
-                    type="email"
-                    placeholder="Email*"
-                    className="form-control p-2"
-                    required
-                  />
-                </div>
-              </div>
               <div className="mb-3">
                 <textarea
                   className="form-control"
                   rows="5"
+                  onChange={(e) => setMitteilung(e.target.value)}
                   placeholder="Mitteilung"
                 ></textarea>
               </div>
-              <button type="submit" className="reservation-btn mt-3 px-5 py-2">
+              <button
+                type="submit"
+                onClick={(e) => createKontakt(currentUser.email)}
+                className="reservation-btn mt-3 px-5 py-2"
+              >
                 SENDEN
               </button>
             </form>
@@ -81,7 +76,7 @@ function Kontakt() {
             </div>
           </div>
         </div>
-        <div className="row mb-5"> 
+        <div className="row mb-5">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10796.709888793464!2d9.1277163!3d47.4279827!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479ae868e8428767%3A0x35ea8a541bb295e!2sRest.%20Pizzeria%20Pinocchio!5e0!3m2!1sen!2sch!4v1683396235925!5m2!1sen!2sch"
             width="600"
